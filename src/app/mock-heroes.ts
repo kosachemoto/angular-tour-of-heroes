fef10df4d388e97ca83f13f9003d618a0fe60c56
Nikita Kosachev <kosachev.a.nikita@gmail.com>
2018-07-23 14:15:43 +0300

    Init Angular app

---
+++ src/app/mock-heroes.ts
@@ -0,0 +1,14 @@
+import { Hero } from './hero';
+
+export const HEROES: Hero[] = [
+  { id: 11, name: "Alvar" },
+  { id: 12, name: "Ludomira" },
+  { id: 13, name: "Sidon" },
+  { id: 14, name: "Laurenc" },
+  { id: 15, name: "Daniel" },
+  { id: 16, name: "Randolf" },
+  { id: 17, name: "Aglaja" },
+  { id: 18, name: "Ria" },
+  { id: 19, name: "Milic" },
+  { id: 20, name: "Radan" }
+];
